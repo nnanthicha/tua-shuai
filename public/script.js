6cad4561689b38d5b8c42186ae905c50d8057363
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2022-04-18 22:35:42 +0700

    update files in public

--- public/script.js
+++ public/script.js
@@ -13,18 +13,23 @@ const app = initializeApp(firebaseConfig);
 
 import {
   setDoc,
+  addDoc,
   collection,
   doc,
   getDoc,
+  getDocs,
   getFirestore,
+  query,
+  where,
 } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
 const db = getFirestore();
+const subjectRef = collection(db, "subjects");
 
 var currentUser;
 var editScheduleModal = document.getElementById("edit-schedule");
 var loginWarningModal = document.getElementById("login-warning");
-var addAssignmentModal = document.getElementById("add-assignment")
+var addAssignmentModal = document.getElementById("add-assignment");
 
 document.getElementById("username-form").addEventListener(
   "submit",
@@ -40,6 +45,7 @@ document.getElementById("username-form").addEventListener(
       currentUser = uid;
       console.log("current user " + currentUser);
       alert("You are login as " + currentUser);
+      redrawSubjectList();
     } else {
       currentUser = null;
       alert("Invalid ID or username");
@@ -56,11 +62,11 @@ document.getElementById("edit-schedule-button").onclick = function () {
   }
 };
 
-document.getElementById("close-schedule-modal-button").onclick = function() {
+document.getElementById("close-schedule-modal-button").onclick = function () {
   editScheduleModal.style.display = "none";
 };
 
-document.getElementById("add-assignment-button").onclick = function() {
+document.getElementById("add-assignment-button").onclick = function () {
   if (currentUser) {
     addAssignmentModal.style.display = "block";
   } else {
@@ -68,7 +74,7 @@ document.getElementById("add-assignment-button").onclick = function() {
   }
 };
 
-document.getElementById("close-assignment-modal").onclick = function() {
+document.getElementById("close-assignment-modal").onclick = function () {
   addAssignmentModal.style.display = "none";
 };
 
@@ -77,8 +83,45 @@ window.onclick = function (event) {
     editScheduleModal.style.display = "none";
   } else if (event.target == loginWarningModal) {
     loginWarningModal.style.display = "none";
-  } else if(event.target == addAssignmentModal) {
-    addAssignmentModal.style.display = "none"
-  } 
+  } else if (event.target == addAssignmentModal) {
+    addAssignmentModal.style.display = "none";
+  }
 };
 
+document
+  .getElementById("subject-form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    let uid = currentUser;
+    let subject = document.getElementById("subject").value;
+    let day = document.getElementById("day").value;
+    let startTime = document.getElementById("starting-time").value;
+    let endTime = document.getElementById("ending-time").value;
+    await addDoc(subjectRef, {
+      uid,
+      subject,
+      day,
+      startTime,
+      endTime,
+    });
+    redrawSubjectList();
+  });
+
+async function redrawSubjectList() {
+  const subjectList = document.getElementById("subject-list");
+  subjectList.innerHTML = "";
+  const q = await getDocs(query(subjectRef, where("uid", "==", currentUser)));
+  q.forEach((sub) => {
+    console.log(sub.id, "=>", sub.data()); 
+    subjectList.innerHTML += `
+    <tr id="${sub.id}">
+      <td>${sub.data().subject}</td>
+      <td>${sub.data().day}</td>
+      <td>${sub.data().startTime} - ${sub.data().endTime}</td>
+    </tr>`;
+  });
+  document.getElementById("subject").value = "";
+  document.getElementById("day").value = "";
+  document.getElementById("starting-time").value = "";
+  document.getElementById("ending-time").value = "";
+}
